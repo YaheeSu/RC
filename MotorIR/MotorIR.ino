cc59f1f9225f121b760b5870e71a97b138718cee
YaheeSu <100844876+YaheeSu@users.noreply.github.com>
2022-11-30 20:28:37 +0800

    Add files via upload

---
+++ MotorIR/MotorIR.ino
@@ -0,0 +1,84 @@
+//IR sensor setting
+int IRR = A0;
+int IRL = A1;
+
+// Motor L
+int INL1 = 7; 
+int INL2 = 6; 
+int ENAL = 11;
+
+// Motor R
+int INR1 = 5; 
+int INR2 = 4; 
+int ENAR = 3; 
+
+//pwm speed
+int PWMspeedHalf = 150;
+int PWMspeedQuator = 75;
+
+void setup(){
+  //setting 
+  Serial.begin(9600);
+
+  //IR sensor pinmode
+  pinMode(IRR, INPUT);
+  pinMode(IRL, INPUT);
+  
+  //INL
+  pinMode(INL1, OUTPUT);
+  pinMode(INL2, OUTPUT);
+  pinMode(ENAL, OUTPUT);
+  
+  //INR
+  pinMode(INR1, OUTPUT);
+  pinMode(INR2, OUTPUT);
+  pinMode(ENAR, OUTPUT);
+}
+
+void loop(){
+  IRValue();
+
+  if (IRL>100 && IRR>100)
+  {
+    digitalWrite(INL1, HIGH);
+    digitalWrite(INL2, LOW);
+    digitalWrite(INR1, HIGH);
+    digitalWrite(INR2, LOW);
+    analogWrite (ENAL, PWMspeedHalf);
+    analogWrite (ENAR, PWMspeedHalf);
+    Serial.println("Forward");
+  }
+  else if (IRL<100 && IRR>100)
+  {
+    digitalWrite(INL1, HIGH);
+    digitalWrite(INL2, LOW);
+    digitalWrite(INR1, HIGH);
+    digitalWrite(INR2, LOW);
+    analogWrite (ENAL, PWMspeedQuator);
+    analogWrite (ENAR, PWMspeedHalf);
+    Serial.println("Left");
+  }
+  else if (IRL>100 && IRR<100)
+  {
+    digitalWrite(INL1, HIGH);
+    digitalWrite(INL2, LOW);
+    digitalWrite(INR1, HIGH);
+    digitalWrite(INR2, LOW);
+    analogWrite (ENAL, PWMspeedHalf);
+    analogWrite (ENAR, PWMspeedQuator);
+    Serial.println("Right");
+  }
+  
+}
+
+void IRValue(){
+  //read the IR sensor value
+  delay(1000);
+  Serial.print("IRR value:");
+  Serial.println(analogRead(IRR));
+  Serial.print("IRL value:");
+  Serial.println(analogRead(IRL));
+  Serial.println(" ");
+  
+  delay(500);
+} 
